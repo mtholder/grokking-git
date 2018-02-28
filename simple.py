55d5d8ac8af3c1063dbff9ad3a473cb2ab733543
Mark T. Holder <mtholder@gmail.com>
2018-02-28 17:43:55 -0600

    counts sequences

--- simple.py
+++ simple.py
@@ -1,5 +1,12 @@
 #!/bin/env python
 from __future__ import print_function
+import sys
 
-print("Hello World\nThis is just a placeholder program.")
+fn = sys.argv[1]
+with open(fn, 'r') as inp:
+    num_sequences = 0
+    for line in inp:
+        if line.startswith('>'):
+            num_sequences += 1
 
+print('{} sequences'.format(num_sequences))
