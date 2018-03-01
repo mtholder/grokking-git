fd74d06d39244ff6af3f237049b189b3b081c705
Mark T. Holder <mtholder@gmail.com>
2018-02-28 18:12:05 -0600

    buggy

--- simple.py
+++ simple.py
@@ -3,10 +3,29 @@ from __future__ import print_function
 import sys
 
 fn = sys.argv[1]
-with open(fn, 'r') as inp:
-    num_sequences = 0
+search_seq = sys.argv[2].upper()
+
+def read_fasta(line_src):
+    name, seq = None, None
     for line in inp:
         if line.startswith('>'):
-            num_sequences += 1
+            if name is not None:
+                yield name, ''.join(seq).strip().upper()
+            name = line[1:].strip()
+            seq = []
+        else:
+            assert seq is not None
+            seq.append(line.strip())
+    yield name, ''.join(seq).strip().upper()
+
+with open(fn, 'r') as inp:
+    num_sequences = 0
+    total_matches = 0
+    for name, seq in read_fasta(inp):
+        num_sequences += 1
+        non_overlapping_splits = seq.split(search_seq)
+        num_matches_this_seq = len(non_overlapping_splits) - 1
 
 print('{} sequences'.format(num_sequences))
+print('{} total matches'.format(total_matches))
+
